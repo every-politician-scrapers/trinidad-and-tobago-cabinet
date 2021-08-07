afc53e375f4125344758686f8ea80896a614d3b3
Tony Bowden <tony@tmtm.com>
2021-08-07 14:12:28 +0300

    Update to every-politician-scraper 0.1.1

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -6,28 +6,22 @@ require 'pry'
 
 class MemberList
   # details for an individual member
-  class Member < Scraped::HTML
-    PREFIXES = %w[Senator Dr The Hon].freeze
-
+  class Member
     REMAP = {
       'Minister of Energy and Energy Industries and Minister in the Office of the Prime Minister' => [
         'Minister of Energy and Energy Industries', 'Minister in the Office of the Prime Minister'
       ],
     }.freeze
 
-    field :name do
-      PREFIXES.reduce(full_name) { |current, prefix| current.sub(/#{prefix}\.? /i, '') }
+    def name
+      Name.new(full: noko.text.tidy, prefixes: %w[Senator Dr The Hon]).short
     end
 
-    field :position do
+    def position
       REMAP.fetch(raw_position, raw_position)
     end
 
     private
-
-    def full_name
-      noko.text.tidy
-    end
 
     def raw_position
       noko.xpath('following::*[contains(., "Minister")][1]').text.tidy
@@ -35,16 +29,7 @@ class MemberList
   end
 
   # The page listing all the members
-  class Members < Scraped::HTML
-    field :members do
-      member_container.flat_map do |member|
-        data = fragment(member => Member).to_h
-        [data.delete(:position)].flatten.map { |posn| data.merge(position: posn) }
-      end
-    end
-
-    private
-
+  class Members
     def member_container
       # There's no real structure here. Everything seems to be in one of
       # <h1>, <h2>, or <p><strong>
